633d4d2fdda78b4bda5b294a1f091922dae66b9a
DESKTOP-ITDK7B5\VINUTA <vinutaks007@gmail.com>
2020-10-18 14:52:07 +0530

    update operators

--- c02/js/add-content.js
+++ c02/js/add-content.js
@@ -23,3 +23,42 @@ function Arithematicoperators()
 	var k = i ++; // post - increment : k equals 2; i equals 3
 	console.log(k+k);
 }
+function Number_and_Strings()
+{
+	var foo = 1;
+    var bar = '2' ;
+	 console.log( foo + bar ); // 12. uh oh
+	 var foo = 1;
+     var bar = '2';
+// coerce the string to a number
+     console.log ( foo + Number ( bar ));
+}
+function Logicaloperators()
+{
+	var foo = 1;
+	var bar = 0;
+	var baz = 2;
+	foo || bar ; // returns 1 , which is true
+	bar || foo ; // returns 1 , which is true
+	foo && bar ; // returns 0 , which is false
+	foo && baz ; // returns 2 , which is true
+	baz && foo ; // returns 1 , which is true
+	console.log(foo+bar);
+}
+function comparisonoperators()
+{
+	var foo = 1;
+	var bar = 0;
+	var baz = '1';
+	var bim = 2;
+	foo == bar ; // returns false
+	foo != bar ; // returns true
+	foo == baz ; // returns true ; careful !
+	foo === baz ; // returns false
+	foo !== baz ; // returns true
+	foo === parseInt ( baz ); // returns true
+	foo > bim ; // returns false
+	bim > baz ; // returns true
+	foo <= baz ; // returns true
+	console.log(foo==baz);
+}
